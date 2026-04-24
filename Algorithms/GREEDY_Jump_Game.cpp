cfb5cba8a8cd4be10bb5ad12078614a9ecf4f83c
nova <hungdevel2007@gmail.com>
2026-04-24 10:48:53 +0700

    add jump game

---
+++ Algorithms/GREEDY_Jump_Game.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+using namespace std;
+// greedy, bottom up
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int goal = nums.size() - 1;
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            if (i + nums[i] >= goal) goal = i;
+        }
+        if (goal == 0) return true;
+        return false;
+    }
+};
